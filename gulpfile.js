d20c72a858f7dc19d9a291c827e54ec74ce64428
wikichen <code@chen.io>
2014-04-23 15:54:43 -0700

    Switch to Gulp for build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,67 @@
 // include gulp
 var gulp = require('gulp');
-var changed = require('gulp-changed');
-var cached = require('gulp-cached');
-var imagemin = require('gulp-imagemin');
-var minifyHTML = require('gulp-minify-html');
+var pkg  = require('./package.json');
 
-// SASS/CSS build plugins
-var sass = require('gulp-sass');
-var autoprefix = require('gulp-autoprefixer');
-var minifyCSS = require('gulp-minify-css');
+// include gulp-specific plugins
+var plugins = require("gulp-load-plugins")();
 
-// JS build plugins
-var jshint = require('gulp-jshint');
-var uglify = require('gulp-uglify');
+// include external plugins not loaded by gulp-load-plugins
+var runseq = require('run-sequence');
 
 // global build variables
 var buildDst  = './_site';
-var assetsSrc = './_assets',
+var assetsSrc = './assets',
     assetsDst = buildDst + '/assets';
-var sassSrc   = assetsSrc + '/stylesheets/*.scss',
-    sassDst   = assetsDst + '/css';
-var jsSrc     = assetsSrc + '/javascripts',
-    jsDst     = assetsDst + '/js';
-var imagesDr  = '/images';
 
-// JS hint task
-gulp.task('jshint', function() {
-  gulp.src('./_assets/javascripts/*.js')
-    .pipe(cached('linting'))
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'));
+// run a LiveReload server and auto open in browser
+
+
+gulp.task('connect', function() {
+  plugins.connect.server({
+    root: [ buildDst ],
+    port: 4000,
+    livereload: true
+  });
+});
+
+// build jekyll site
+gulp.task('jekyll-build', function() {
+  return gulp.src('')
+    .pipe(plugins.exec("jekyll build"));
+});
+
+// reloads when an HTML file is changed and runs jekyll task
+gulp.task('html', ['jekyll-build'], function() {
+  return gulp.src(['./**/*.html', '!./node_modules/**/*'])
+    .pipe(plugins.connect.reload());
+});
+
+// reloads when a Markdown file is changed and runs jekyll task
+gulp.task('markdown', ['jekyll-build'], function() {
+  return gulp.src('./**/*.md')
+    .pipe(plugins.concat.reload());
+});
+
+// lint task
+gulp.task('lint', function() {
+  return gulp.src('./_assets/js/*.js')
+    .pipe(plugins.cached('linting'))
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('default'));
+});
+
+// concatenate & minify js
+gulp.task('scripts', function() {
+  var src = assetsSrc + '/js/*.js',
+      dst = assetsDst + '/js';
+
+  return gulp.src(src)
+    .pipe(plugins.concat('all.js'))
+    .pipe(gulp.dest(dst))
+    .pipe(plugins.rename('all.min.js'))
+    .pipe(plugins.uglify())
+    .pipe(plugins.size())
+    .pipe(gulp.dest(dst));
 });
 
 // minify new images
@@ -37,7 +69,7 @@ gulp.task('imagemin', function() {
   var imgSrc = './uploads/**/*',
       imgDst = imgSrc;
 
-  gulp.src(imgSrc)
+  return gulp.src(imgSrc)
     .pipe(changed(imgDst))
     .pipe(imagemin())
     .pipe(gulp.dest(imgDst));
@@ -48,19 +80,55 @@ gulp.task('htmlmin', function() {
   var htmlSrc = './_site/**/*.html',
       htmlDst = htmlSrc;
 
-  gulp.src(htmlSrc)
-    .pipe(changed(htmlDst))
-    .pipe(minifyHTML())
+  return gulp.src(htmlSrc)
+    .pipe(plugins.changed(htmlDst))
+    .pipe(plugins.minifyHtml())
     .pipe(gulp.dest(htmlDst));
 });
 
 // compile SASS files
 gulp.task('sass', function() {
-  var src = sassSrc, dst = sassDst;
+  var src = assetsSrc + '/scss/*.scss',
+      dst = assetsSrc + '/css';
 
-  gulp.src(src)
-    .pipe(sass())
-    .pipe(autoprefix('last 2 versions'))
-    .pipe(minifyCSS())
+  return gulp.src(src)
+    .pipe(plugins.changed(src))
+    .pipe(plugins.sass({
+        noCache: true,
+        quiet: true
+      }))
+    //.pipe(plugins.autoprefixer('last 2 versions'))
+    .pipe(plugins.minifyCss())
     .pipe(gulp.dest(dst));
+});
+
+// LiveReload for CSS
+gulp.task('css', function(){
+  var src = assetsSrc + '/css/*.css';
+
+  return gulp.src(src)
+    .pipe(plugins.changed(assetsSrc + '/css/**/*'))
+    .pipe(gulp.dest(assetsDst + '/css/'))
+    .pipe(plugins.size())
+    .pipe(plugins.connect.reload());
+});
+
+// watch files for changes, exclude node_modules
+gulp.task('watch', function() {
+  gulp.watch(['./**/*.html', '!./node_modules/**/*'], ['html']);
+  gulp.watch('./_posts/**/*.md', ['markdown']);
+  gulp.watch(assetsSrc + '/scss/**/*.scss', ['sass']);
+  gulp.watch(assetsSrc + '/css/**/*.css', ['css']);
+  gulp.watch(assetsSrc + '/js/*.js', ['lint', 'scripts']);
+});
+
+gulp.task('clean', function() {
+  return gulp.src('./_site/**/*', { read: false })
+    .pipe(plugins.clean());
+});
+
+// default task
+//gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
+gulp.task('default', ['clean', 'jekyll-build', 'sass', 'css'], function() {
+  return gulp.start('connect', 'watch');
 });
